7da9e85b199e7b9a69c78a436f421ebe9207e0fb
Joshua Fayele <98998174+JoshuaFayele@users.noreply.github.com>
2023-11-17 10:03:35 +0100

    update

--- multi_app/eda_app.py
+++ multi_app/eda_app.py
@@ -22,6 +22,7 @@ def eda_app():
     df = load_data("data/diabetes_data_upload.csv")
     # st.dataframe(df)
     df_encoded = load_data("data/diabetes_data_upload_clean.csv")
+    freq_df = load_data("data/freqdist_of_age_data.csv")
     
     submenu = st.sidebar.selectbox("Sub Menu", ["Descriptive", "Plots"])
     if submenu == "Descriptive":
@@ -46,6 +47,7 @@ def eda_app():
         col1, col2 = st.columns([2,1])
         
         with col1:
+            # Gender Distribution
             with st.expander("Distribution Plot of Gender"):
                 # Using Seaborn
                 #fig = plt.figure()
@@ -56,13 +58,53 @@ def eda_app():
                 # ax.scatter([1, 2, 3], [1, 2, 3])
                 # st.pyplot(fig)
                 fig = plt.figure()
-                df['Gender'].value_counts().plot(kind='bar', color=['orange', 'green'])
+                df['Gender'].value_counts().plot(kind='bar', color=['blue', 'orange'])
                 st.pyplot(fig)
                 
                 gen_df = df['Gender'].value_counts().to_frame()
                 gen_df = gen_df.reset_index()
                 gen_df.columns = ["Gender Type", "Counts"]
-                st.dataframe(gen_df)
+                # st.dataframe(gen_df)
                 
                 p1 = px.pie(gen_df, names='Gender Type', values='Counts')
-                st.plotly_chart(p1)
+                st.plotly_chart(p1, use_container_width=True)
+                
+            # For Class Distribution
+            with st.expander("Distribution Plot of Class"):
+                fig = plt.figure()
+                df['class'].value_counts().plot(kind='bar',  color=['blue', 'orange'])
+                st.pyplot(fig)
+                
+        with col2:
+            with st.expander("Gender Distribution"):
+                st.dataframe(gen_df)
+                
+            with st.expander("Class Distribution"):
+                st.dataframe(df['class'].value_counts())
+        
+        
+        # Freq Dist
+        with st. expander("Frequency Distribution of Age"):
+            st.dataframe(freq_df)
+            p2 = px.bar(freq_df, x='Age', y='count')
+            st.plotly_chart(p2)
+            
+        # Outlier Detection
+        with st.expander("Outlier Detection"):
+            fig = plt.figure()
+            sns.boxplot(df['Age'])
+            st.pyplot(fig)
+            
+            p3 = px.box(df, x='Age', color='Gender')
+            st.plotly_chart(p3)
+        
+        # Correlation
+        with st.expander("Correlation Plot"):
+            corr_matrix = df_encoded.corr()
+            fig = plt.figure(figsize=(20,10))
+            sns.heatmap(corr_matrix, annot=True)
+            st.pyplot(fig)
+            
+            p4 = px.imshow(corr_matrix)
+            st.plotly_chart(p4)
+            
